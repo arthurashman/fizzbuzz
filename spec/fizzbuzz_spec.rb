47519377f6d359f3ec810064b9027fb82dd978de
Arthur Ashman <arthurashman@gmail.com>
2019-02-26 14:16:28 +0000

    refactored code to include multiples

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,29 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
   expect(fizzbuzz(3)).to eq "fizz"
+  expect(fizzbuzz(6)).to eq "fizz"
   end
 end
 
 describe 'fizzbuzz' do
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when passed a multiple of 5' do
   expect(fizzbuzz(5)).to eq "buzz"
+  expect(fizzbuzz(10)).to eq "buzz"
 end
 end
 
 describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when passed a multiple of 15' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
   end
+end
+
+describe 'fizzbuzz' do
+  it 'returns number when passed any non-fizzbuzz number' do
+  expect(fizzbuzz(4)).to eq 4
+  expect(fizzbuzz(7)).to eq 7
+end
 end
